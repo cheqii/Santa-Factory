dbc6e829295d1ccf9979eb4fa909485f1cdcaa77
qiqitaro <thanajan.saes@bumail.net>
2023-12-22 16:35:46 +0700

    random child mail between good and bad kids

--- Assets/Scripts/Mail.cs
+++ Assets/Scripts/Mail.cs
@@ -9,21 +9,53 @@ public class Mail : MonoBehaviour
 
     [Header("Children Data List")]
     [SerializeField] private List<Child> blackListChild;
-    [SerializeField] private List<Child> goodChild;
-    
-    [SerializeField] private List<Child> allChildList;
 
-    [SerializeField] public string childNameMail;
+    public List<Child> BlackListChild
+    {
+        get => blackListChild;
+        set => blackListChild = value;
+    }
+    
+    [SerializeField] private List<Child> goodChild;
+
+    public List<Child> GoodChild
+    {
+        get => goodChild;
+        set => goodChild = value;
+    }
+
+    private int randomChildChance;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        RandomChildMail();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            RandomChildMail();
+        }
+    }
+
+    public void RandomChildMail()
+    {
+        randomChildChance = Random.Range(0, 10);
+        var childIndex = Random.Range(0, 13); // to random child name
+        if (randomChildChance <= 5)
+        {
+            // use child from good list
+            childNameText.color = Color.black;
+            childNameText.text = goodChild[childIndex].name;
+        }
+        else if (randomChildChance > 5)
+        {
+            // use child from blacklist
+            childNameText.color = Color.red;
+            childNameText.text = blackListChild[childIndex].name;
+        }
     }
 }
